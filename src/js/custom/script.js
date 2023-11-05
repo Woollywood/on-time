8fef3821ecb74c5784f0b7e0c0804ad947dffbab
woollywood <Gerbert.Wells969@yandex.ru>
2023-11-05 03:07:53 +0300

    desktopScrollSync and template

--- src/js/custom/script.js
+++ src/js/custom/script.js
@@ -35,15 +35,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	}
 });
 
-window.addEventListener('load', (event) => {
-	// layerScrollTemplateInit(document.querySelector('[scroll-block-wrapper]'), document.querySelector('[scroll-block]'));
-});
-
-function layoutGapObserver() {
-	const wrapper = document.querySelector('[data-wrapper]');
-	wrapper.style.cssText += `--top-gap: ${wrapper.getBoundingClientRect().top}px`;
-}
-
 window.addEventListener('load', (windowEvent) => {
 	document.addEventListener('click', (clickEvent) => {
 		const targetElement = clickEvent.target;
@@ -54,7 +45,38 @@ window.addEventListener('load', (windowEvent) => {
 	});
 });
 
-function layerScrollTemplateInit(scrollBlockWrapper, scrollBlock) {
+window.addEventListener('load', (e) => {
+	scrollInit();
+});
+
+function scrollInit() {
+	const scrollBlocks = document.querySelectorAll('[scroll-block-wrapper]');
+	scrollBlocks.forEach((scrollBlock) => {
+		if (scrollBlock.getAttribute('scroll-sync')) {
+			const syncBlocks = scrollBlock
+				.getAttribute('scroll-sync')
+				.split(',')
+				.map((string) => string.trim())
+				.map((selector) => document.querySelector(selector));
+
+			scrollSyncInit(scrollBlock, syncBlocks);
+		}
+
+		layerScrollTemplateInit(scrollBlock);
+	});
+}
+
+function scrollSyncInit(scrollBlock, syncBlocks) {
+	scrollBlock.addEventListener('layout-scroll', (scrollEvent) => {
+		syncBlocks.forEach((block) =>
+			moveBlockSync(block, scrollEvent.detail.scrollLeft, scrollEvent.detail.scrollTop)
+		);
+	});
+}
+
+function layerScrollTemplateInit(scrollBlockWrapper) {
+	const scrollBlock = scrollBlockWrapper.firstElementChild;
+
 	let leftStart = 0;
 	let leftEnd = 0;
 	let scrollX = 0;
@@ -66,6 +88,13 @@ function layerScrollTemplateInit(scrollBlockWrapper, scrollBlock) {
 	let scrollableY = scrollBlock.offsetHeight - scrollBlockWrapper.offsetHeight;
 
 	let drag = false;
+
+	const scrollEvent = new CustomEvent('layout-scroll', {
+		detail: {
+			scrollLeft: 0,
+			scrollTop: 0,
+		},
+	});
 
 	window.addEventListener('resize', (resizeEvent) => {
 		scrollableX = scrollBlock.offsetWidth - scrollBlockWrapper.offsetWidth;
@@ -79,6 +108,8 @@ function layerScrollTemplateInit(scrollBlockWrapper, scrollBlock) {
 	}
 
 	function pointerMove(e) {
+		console.log(drag);
+
 		if (drag) {
 			let diffX = -(leftEnd + e.pageX - leftStart);
 
@@ -99,6 +130,10 @@ function layerScrollTemplateInit(scrollBlockWrapper, scrollBlock) {
 			} else {
 				scrollY = -diffY;
 			}
+
+			scrollEvent.detail.scrollLeft = scrollX;
+			scrollEvent.detail.scrollTop = scrollY;
+			scrollBlockWrapper.dispatchEvent(scrollEvent);
 
 			moveBlock(scrollBlock, scrollX, scrollY);
 		}
@@ -122,149 +157,56 @@ function layerScrollTemplateInit(scrollBlockWrapper, scrollBlock) {
 		}
 	}
 
-	function moveBlock(scrollBlock, scrollX, scrollY) {
-		scrollBlock.style.cssText += `
-				transition-duration: 0ms;
-				transition-delay: 0ms;
-				transform: translate3d(${scrollX}px, ${scrollY}px, 0px);
-			`;
-	}
-
 	scrollBlock.addEventListener('pointerup', pointerUp);
 	scrollBlock.addEventListener('pointermove', pointerMove);
 	scrollBlock.addEventListener('pointerdown', pointerDown);
 }
 
-// function layerScrollTemplateInit(scrollBlockWrapper, scrollBlock) {
-// 	let wrapperContainer = document.querySelector('.wrapper-container');
-// 	let paddingRight = parseInt(getComputedStyle(wrapperContainer, true).paddingRight);
+function moveBlockSync(scrollBlockWrapper, scrollX, scrollY) {
+	const scrollBlock = scrollBlockWrapper.firstElementChild;
 
-// 	if (scrollBlock.hasAttribute('data-scroll-y-stop')) {
-// 		allowScrollY = false;
-// 	} else {
-// 		allowScrollY = true;
-// 	}
+	const scrollXAllowed = getAllowedXScroll(scrollBlock, scrollBlockWrapper, scrollX);
+	const scrollYAllowed = getAllowedYScroll(scrollBlock, scrollBlockWrapper, scrollY);
 
-// 	let leftStart = 0;
-// 	let leftEnd = 0;
-// 	let scrollX = 0;
-// 	let scrollableX = allowScrollX
-// 		? scrollBlock.clientWidth - (window.innerWidth - scrollBlock.getBoundingClientRect().x - paddingRight)
-// 		: 0;
+	scrollBlock.style.cssText += `
+			transition-duration: 0ms;
+			transition-delay: 0ms;
+			transform: translate3d(${scrollXAllowed}px, ${scrollYAllowed}px, 0px);
+		`;
+}
 
-// 	let topStart = 0;
-// 	let topEnd = 0;
-// 	let scrollY = 0;
-// 	let scrollableY = allowScrollY
-// 		? scrollBlock.clientHeight - (wrapperContainer.clientHeight - scrollBlock.getBoundingClientRect().y)
-// 		: 0;
+function getAllowedXScroll(scrollBlock, scrollBlockWrapper, parentScrollValue) {
+	const scrollValue = scrollBlock.offsetWidth - scrollBlockWrapper.offsetWidth;
+	const scrollAllowed =
+		Math.abs(parentScrollValue) > scrollValue
+			? parentScrollValue - (parentScrollValue + scrollValue)
+			: parentScrollValue;
 
-// 	if (scrollableY < 0) {
-// 		scrollableY = 0;
-// 	}
+	return scrollAllowed;
+}
 
-// 	let drag = false;
+function getAllowedYScroll(scrollBlock, scrollBlockWrapper, parentScrollValue) {
+	const scrollValue = scrollBlock.offsetHeight - scrollBlockWrapper.offsetHeight;
+	const scrollAllowed =
+		Math.abs(parentScrollValue) > scrollValue
+			? parentScrollValue - (parentScrollValue + scrollValue)
+			: parentScrollValue;
 
-// 	window.addEventListener('resize', (resizeEvent) => {
-// 		scrollableX = allowScrollX
-// 			? scrollBlock.clientWidth - (window.innerWidth - scrollBlock.getBoundingClientRect().x - paddingRight)
-// 			: 0;
+	return scrollAllowed;
+}
 
-// 		scrollableY = allowScrollY
-// 			? scrollBlock.clientHeight - (wrapperContainer.clientHeight - scrollBlock.getBoundingClientRect().y)
-// 			: 0;
-// 	});
+function moveBlock(scrollBlock, scrollX, scrollY) {
+	scrollBlock.style.cssText += `
+			transition-duration: 0ms;
+			transition-delay: 0ms;
+			transform: translate3d(${scrollX}px, ${scrollY}px, 0px);
+		`;
+}
 
-// 	function mouseUp(e) {
-// 		drag = false;
-
-// 		if (allowScrollX) {
-// 			leftEnd += e.pageX - leftStart;
-// 			if (leftEnd > 0) {
-// 				leftEnd = 0;
-// 			} else if (leftEnd <= -scrollableX) {
-// 				leftEnd = -scrollableX;
-// 			}
-// 		}
-
-// 		if (allowScrollY) {
-// 			topEnd += e.pageY - topStart;
-// 			if (topEnd > 0) {
-// 				topEnd = 0;
-// 			} else if (topEnd <= -scrollableY) {
-// 				topEnd = -scrollableY;
-// 			}
-// 		}
-
-// 		setInterval(() => {
-// 			inertScroll(scrollBlock, allowScrollX, false);
-// 			dependentBlocks?.forEach((block) => {
-// 				inertScroll(block.elem, block.allowScrollX, false);
-// 			});
-// 		}, 30);
-// 	}
-
-// 	function inertScroll(scrollBlock, allowScrollX, allowScrollY) {
-// 		if (allowScrollX) scrollX -= 10;
-// 		if (allowScrollY) scrollY -= 10;
-
-// 		if (-scrollX > scrollableX) scrollX = -scrollableX;
-// 		if (-scrollY > scrollableY) scrollY = -scrollableY;
-
-// 		moveBlock(scrollBlock, allowScrollX, scrollX, allowScrollY, scrollY);
-// 	}
-
-// 	function moveBlock(scrollBlock, allowScrollX, scrollX, allowScrollY, scrollY) {
-// 		scrollBlock.style.cssText += `
-// 				transition-duration: 0ms;
-// 				transition-delay: 0ms;
-// 				transform: translate3d(${allowScrollX ? scrollX : 0}px, ${allowScrollY ? scrollY : 0}px, 0px);
-// 			`;
-// 	}
-
-// 	function mouseMove(e) {
-// 		if (drag) {
-// 			if (allowScrollX) {
-// 				let diffX = -(leftEnd + e.pageX - leftStart);
-
-// 				if (diffX <= 0) {
-// 					scrollX = 0;
-// 				} else if (diffX > scrollableX) {
-// 					scrollX = -scrollableX;
-// 				} else {
-// 					scrollX = -diffX;
-// 				}
-// 			}
-
-// 			if (allowScrollY) {
-// 				let diffY = -(topEnd + e.pageY - topStart);
-
-// 				if (diffY <= 0) {
-// 					scrollY = 0;
-// 				} else if (diffY > scrollableY) {
-// 					scrollY = -scrollableY;
-// 				} else {
-// 					scrollY = -diffY;
-// 				}
-// 			}
-
-// 			moveBlock(scrollBlock, allowScrollX, scrollX, allowScrollY, scrollY);
-// 			dependentBlocks?.forEach((block) => {
-// 				moveBlock(block.elem, block.allowScrollX, scrollX, block.allowScrollY, scrollY);
-// 			});
-// 		}
-// 	}
-
-// 	function mouseDown(e) {
-// 		drag = true;
-// 		leftStart = e.pageX;
-// 		topStart = e.pageY;
-// 	}
-
-// 	scrollBlock.addEventListener('mouseup', mouseUp);
-// 	scrollBlock.addEventListener('mousemove', mouseMove);
-// 	scrollBlock.addEventListener('mousedown', mouseDown);
-// }
+function layoutGapObserver() {
+	const wrapper = document.querySelector('[data-wrapper]');
+	wrapper.style.cssText += `--top-gap: ${wrapper.getBoundingClientRect().top}px`;
+}
 
 function projectItemCreate(id, status, project, supervisor, customerName, address) {
 	console.log('project create start...');
